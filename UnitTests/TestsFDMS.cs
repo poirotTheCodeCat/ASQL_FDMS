3758be6f4b82c0d1ae96d46ef882ed746ea3572b
Emorataya-cc <56742950+Emorataya-cc@users.noreply.github.com>
2020-12-07 14:05:47 -0500

    Update TestsFDMS.cs

--- UnitTests/TestsFDMS.cs
+++ UnitTests/TestsFDMS.cs
@@ -23,5 +23,29 @@ namespace UnitTests
             }
 
         }
+        
+        
+        
+        [TestMethod]
+        public void GetTailTest()
+        {
+            string telFile = "D:\\School Work\\Year 3\\Advance Software Quality\\ASQL_FDMS\\FDMS_Aircraft_Transmission_System\\C-FGAX.txt";
+
+            // regex to match the aircraft tail from the filename
+            Regex aircraftTailRegex = new Regex("(C-.*)[^.txt]");
+            var aircraftMatch = aircraftTailRegex.Match(telFile);
+            // contains the aircraft tail 
+            string aircraftTail = aircraftMatch.Groups[0].ToString();
+
+            Assert.IsTrue(aircraftTail == "C-FGAX", "TestGetTail has Failed");
+        }
+        
+        
+        
+        
+        
+        
+        
+        
     }
 }
